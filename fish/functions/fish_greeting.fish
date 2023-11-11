d205d728814c9f5f8671cff460e01f7023c438f2
medeiros <daniel@arneam.com>
2023-11-11 19:55:42 -0300

    Correct typo

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,7 +1,6 @@
 function fish_greeting
 	set cowsay_options (cowsay -l | tail +2 | tr -d '\n' | string split ' ')
 	set random_idx (random 1 (count $cowsay_options))
-	fortune -e computers startrek linux | \
-		cowsay -f $cowsay_options[$random_idx]
+	fortune -e computers startrek linux | cowsay -f $cowsay_options[$random_idx]
 end
 
